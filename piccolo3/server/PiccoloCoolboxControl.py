d4fcb3651b6a1c22b58fd2803724907825225833
Murray Hoggett <murrayhoggett@gmail.com>
2020-12-13 15:52:42 +0000

    removed coapResources for serial connection

--- piccolo3/server/PiccoloCoolboxControl.py
+++ piccolo3/server/PiccoloCoolboxControl.py
@@ -373,10 +373,6 @@ class PiccoloCoolboxControl(PiccoloBaseComponent):
             self.temperature_sensors[temp] = PiccoloTemperature(
                 temp, serial_connection=self.serial_connection, target=coolbox_cfg['temperature_sensors'][temp]['target'])
 
-        for serial in self.serial_connection:
-            self.coapResources.add_resource(
-                [serial], self.serial_connection[serial].coapResources)
-
         for fan in self.fan_sensors:
             self.coapResources.add_resource(
                 [fan], self.fan_sensors[fan].coapResources)
